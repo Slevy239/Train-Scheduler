fd9594d9d620db1c14e2292721c8ffc389a55f74
Sam Levy <slevy239@gmail.com>
2019-09-26 23:37:53 -0400

    format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(document).ready(function () {
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 
 
-  $("#submit").on("click", function(event) {
+  $("#submit").on("click", function (event) {
     event.preventDefault();
     title = $("#name-input").val().trim();
     trainDestination = $("#destination-input").val().trim();
@@ -31,10 +31,10 @@ $(document).ready(function() {
 
     // Pushing to database
     database.ref().push({
-        title: title,
-        trainDestination: trainDestination,
-        trainFrequency: trainFrequency,
-        start:start,
+      title: title,
+      trainDestination: trainDestination,
+      trainFrequency: trainFrequency,
+      start: start,
     });
 
     $("#name-input").val("");
@@ -43,10 +43,10 @@ $(document).ready(function() {
     $("#first-input").val("");
 
     alert("The " + title + " Train has been added!");
-});
+  });
 
-database.ref().on("child_added", function(childSnapshot) {
-  // console.log(childSnapshot.val());
+  database.ref().on("child_added", function (childSnapshot) {
+    // console.log(childSnapshot.val());
 
 
 
@@ -54,36 +54,36 @@ database.ref().on("child_added", function(childSnapshot) {
     var startTimeConvert = moment(childSnapshot.val().start, "hh:mm").subtract(1, "years");
     var difference = moment().diff(moment(startTimeConvert), "minutes"); //showing the difference between now and the start input.
     var timeRemaining = difference % childSnapshot.val().trainFrequency;// remainder of difference/trainFrequency;
-    var nextToArrive =childSnapshot.val().trainFrequency - timeRemaining; //based on train frequency, it is subtracted by the time remaining to show the time until next train
+    var nextToArrive = childSnapshot.val().trainFrequency - timeRemaining; //based on train frequency, it is subtracted by the time remaining to show the time until next train
     var next = moment().add(nextToArrive, "minutes") //adding current time to time remaing and reformatting
-    
+
     var newRow = $("<tr>").append(
-        $("<td>").text(childSnapshot.val().title),
-        $("<td>").text(childSnapshot.val().trainDestination),
-        $("<td>").text(childSnapshot.val().trainFrequency),
-        $("<td>").text(moment(next).format("HH:mm")),
-        $("<td>").text(nextToArrive)
-        );
-        
-        $("#train-table > tbody").append(newRow);
+      $("<td>").text(childSnapshot.val().title),
+      $("<td>").text(childSnapshot.val().trainDestination),
+      $("<td>").text(childSnapshot.val().trainFrequency),
+      $("<td>").text(moment(next).format("HH:mm")),
+      $("<td>").text(nextToArrive)
+    );
+
+    $("#train-table > tbody").append(newRow);
 
 
-          (function () {
-            function checkTime(i) {
-                return (i < 10) ? "0" + i : i;
-            }
-            function startTime() {
-                var today = new Date(),
-                    h = checkTime(today.getHours()),
-                    m = checkTime(today.getMinutes()),
-                    s = checkTime(today.getSeconds());
-                document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-                t = setTimeout(function () {
-                    startTime()
-                }, 500);
-            }
-            startTime();
-        })();
+    (function () {
+      function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+      }
+      function startTime() {
+        var today = new Date(),
+          h = checkTime(today.getHours()),
+          m = checkTime(today.getMinutes()),
+          s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+          startTime()
+        }, 500);
+      }
+      startTime();
+    })();
 
-      });
-    });
+  });
+});
